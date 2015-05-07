317ea4424bfe01942ed771b402b23a61f405af1e
Strikingwolf <Strikingwolf2012@gmail.com>
2015-05-07 16:30:07 -0500

    Fix problems with config

--- src/main/java/pt/uptodate/handlers/Config.java
+++ src/main/java/pt/uptodate/handlers/Config.java
@@ -12,7 +12,7 @@ public class Config {
 	public static Configuration configuration;
 
 	public static boolean chat = false;
-	public static boolean severe = false;
+	public static boolean severe = true;
 
 	public static void init(File configFile) {
 
@@ -35,8 +35,9 @@ public class Config {
 
 	private static void loadConfiguration()
 	{
-		chat = configuration.getBoolean("chat", Configuration.CATEGORY_GENERAL, false, "Setting this to true will turn on chat update notifications");
-		severe = configuration.getBoolean("severe", Configuration.CATEGORY_GENERAL, false, "Setting this to false will turn off severe updates stopping loading");
+		// TODO implement marquee, then remove boolean
+		boolean chat = configuration.getBoolean("chat", Configuration.CATEGORY_GENERAL, false, "Setting this to true will turn on chat update notifications (currently disabled due to no marquee)");
+		severe = configuration.getBoolean("severe", Configuration.CATEGORY_GENERAL, true, "Setting this to false will turn off severe updates stopping loading");
 
 		if (configuration.hasChanged())
 		{
