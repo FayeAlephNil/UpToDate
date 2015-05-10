a97b03a59426fe7eeddccb90175b2a2153808156
Strikingwolf <Strikingwolf2012@gmail.com>
2015-05-09 19:57:02 -0500

    Add package info

---
+++ src/main/java/pt/api/package-info.java
@@ -0,0 +1,5 @@
+@API(owner = UpToDate.MOD_NAME, provides = UpToDate.MOD_NAME + "API", apiVersion = UpToDate.VERSION)
+package pt.api;
+
+import cpw.mods.fml.common.API;
+import pt.uptodate.UpToDate;
